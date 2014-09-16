c55ee3d90e400564891250c702dcc4de52949739
Laurens Coderre <lcoderre@me.com>
2014-09-15 20:00:00 -0400

    Added read/write API for logs

--- app/routes/logs.js
+++ app/routes/logs.js
@@ -3,5 +3,8 @@
 module.exports = function(app) {
     // Home route
     var logs = require('../controllers/logs.js');
-    app.get('/logs', logs.render);
+
+    app.get('/logs', logs.read);
+    app.get('/logs/:deviceId', logs.read);
+    app.post('/logs', logs.create);
 };
