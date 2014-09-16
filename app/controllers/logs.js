720ee143aae905272629d41776527d6eb58a6bb2
Laurens Coderre <lcoderre@me.com>
2014-09-15 20:04:09 -0400

    Routes all in one file

--- app/controllers/logs.js
+++ app/controllers/logs.js
@@ -33,9 +33,3 @@ exports.create = function(req, res) {
 		}
 	});
 };
-
-
-
-
-
-
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
     // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
+
+
+    var logs = require('../controllers/logs.js');
+    app.get('/logs', logs.read);
+    app.get('/logs/:deviceId', logs.read);
+    app.post('/logs', logs.create);
 };
