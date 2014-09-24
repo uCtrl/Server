894bc706eab274f7b48ff160dbfb67dd25e3dffa
thomas <thomas.duhautois@usherbrooke.ca>
2014-09-24 10:52:32 -0400

    Implemented the device.js file. Created the platform.js file and implemented route.js

--- app/controllers/device.js
+++ app/controllers/device.js
@@ -3,35 +3,154 @@ var _ = require('lodash');
 var NinjaBlocks = require('../apis/ninjablocks.js');
 var ninja = new NinjaBlocks({userAccessToken:"107f6f460bed2dbb10f0a93b994deea7fe07dad5"});
 
+var TemperatureId;
+var HumidityId;
+var StatuslightId;
+var OnBoardLedId;
+var NinjaEyesId;
+var RfDevicesId;
+
 exports.get = function(req, res) {
+	
+
 	var output = {
-		"messageType": 4,
-		"status": true,
-		"error" : null,
-		"size": 0,
+		"numberofdevices": 0,
 		"devices": []
 	};
 	
 	ninja.devices(function(err, result){
 		var size = 0;
-		console.log(result);
+
+
 		_.each(result, function(el, index){
 			size++;
+			
 			output.devices.push({
 				"id": index,
-				"maxValue": 0,
-				"minValue": 0,
 				"name": el.default_name,
-				"precision": 0,			//TODO
 				"type": el.device_type,	//TODO : translate
-				"unitLabel": el.unit,	//TODO : translate
-				"isTriggerValue": false	//TODO : ?
+			});
+
+			if (el.default_name == "Temperature"){
+				TemperatureId = index;
+			}
+			else if (el.default_name == "Humidity"){
+				HumidityId = index;
+			}
+			else if (el.default_name == "Status Light"){
+				StatuslightId = index;
+			}
+			else if (el.default_name == "On Board RGB LED"){
+				OnBoardLedId = index;
+			}
+			else if (el.default_name == "Nina's Eyes"){
+				NinjaEyesId = index;
+			}
+			else if (el.default_name == "RF 433Mhz"){
+				RfDevicesId = index;
+			}
+
+		});
+
+		output.numberofdevices = size;
+		res.json(output);
+	});
+
+};
+
+
+
+
+exports.getTemperature = function(req, res) {
+	
+
+	var output = {
+		"TemperatureId": TemperatureId,
+		"device": []
+	};
+	
+	ninja.device(TemperatureId, function(err, result){
+		var size = 0;
+
+
+		_.each(result, function(el, index){
+			size++;
+			
+			output.device.push({
+				"id": TemperatureId,
+				"vid": el.vid,
+				"did": el.did,
+				"gid": el.gid,
+				"guid": el.guid,
+				"node": el.node,
+				"device_type": el.device_type,
+				"default_name": el.default_name,
+				"shortName": el.shortName,
+				"tags": el.tags,
+				"is_sensor": el.is_sensor,
+				"is_actuator": el.is_actuator,
+				"is_silent": el.is_silent,
+				"has_time_series": el.has_time_series,
+				"has_subdevice_count": el.has_subdevice_count,
+				"has_state": el.has_state,
+				"unit": el.unit,
+				"documentation": el.documentation,
+				"last_data": el.last_data
 			});
 		});
 		output.size = size;
 		res.json(output);
 	});
+
 };
+
+
+
+exports.getNinjaEyes = function(req, res) {
+	
+
+	var output = {
+		"NinjaEyesId": NinjaEyesId,
+		"device": []
+	};
+	
+	ninja.device(NinjaEyesId, function(err, result){
+		var size = 0;
+
+
+		_.each(result, function(el, index){
+			size++;
+			
+			output.device.push({
+				"id": NinjaEyesId,
+				"vid": el.vid,
+				"did": el.did,
+				"gid": el.gid,
+				"guid": el.guid,
+				"node": el.node,
+				"device_type": el.device_type,
+				"default_name": el.default_name,
+				"shortName": el.shortName,
+				"tags": el.tags,
+				"is_sensor": el.is_sensor,
+				"is_actuator": el.is_actuator,
+				"is_silent": el.is_silent,
+				"has_time_series": el.has_time_series,
+				"has_subdevice_count": el.has_subdevice_count,
+				"has_state": el.has_state,
+				"unit": el.unit,
+				"documentation": el.documentation,
+				"last_data": el.last_data
+			});
+		});
+		output.size = size;
+		res.json(output);
+	});
+
+};
+
+
+
 
 /*
 var TRANSLATOR = {
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -13,19 +13,47 @@ module.exports = function(app) {
 
 	//System
 	//Platform
-	//Device
-	var device = require('../controllers/device.js');
-    app.get('/device', device.get);
-	//Scenario
+    var platform = require('../controllers/platform.js');
+        // Route to get Block informations (ID)
+        app.get('/platform', platform.get); 
+    
+	
+    //Device
+    var device = require('../controllers/device.js');
+        // Route to get all the devices connected (ID + Name + type)
+        app.get('/device', device.get);
+        // Route to get the temperature device informations
+        app.get('/device/temperature', device.getTemperature); 
+        // Route to get the ninja eyes informations
+        app.get('/device/ninjaEyes', device.getNinjaEyes);
+        // Route to get the humidity device informations
+     /* app.get('/device/humidity', device.getHumidity); 
+        // Route to get the statuslight  informations
+        app.get('/device/statuslight', device.getStatuslight);  
+        // Route to get the on board led  informations
+        app.get('/device/onBoardLed', device.getOnBoardLed);  
+        app.get('/device/rfdevices', device.getRfDevices); 
+        */
+
+	    
+
+
+    //Scenario
 	var scenario = require('../controllers/scenario.js');
     app.get('/scenario', scenario.get);
     app.get('/scenario/:deviceId', scenario.get);
-	//Task
+	
+
+    //Task
 	var task = require('../controllers/task.js');
     app.get('/task', task.get);
     app.get('/task/:scenarioId', task.get);
-	//Condition
+	
+
+    //Condition
 	var condition = require('../controllers/condition.js');
     app.get('/condition', condition.get);
     app.get('/condition/:taskId', condition.get);
+
+
 };
