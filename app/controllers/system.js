b6a0935053022d52bc3ed1a846d1f0aa498d2f70
jpgauthier <jp.gauthier0@gmail.com>
2014-10-07 14:35:19 -0400

    Cleaned system json

--- app/controllers/system.js
+++ app/controllers/system.js
@@ -9,7 +9,39 @@ var _ = require('lodash'),
 	UCondition = mongoose.model('UCondition');
 
 exports.all = function(req, res) {
-	function sendError (err) {
+
+	var mapPrivToPub = {
+	    "_devices": "devices",
+	    "_scenarios": "scenarios",
+	    "_tasks": "tasks",
+	    "_conditions": "conditions"
+	};
+
+	var changePrivToPub = function (obj) {
+		if (_.isArray(obj)) {
+			for (var i = 0; i < obj.length; i++) {
+				changePrivToPub(obj[i]);
+			}
+		} else if (_.isPlainObject(obj)) {
+			for (var key in obj) {
+		       	if (obj.hasOwnProperty(key)) {
+		       		var pubKey = mapPrivToPub[key], object;
+		       		if (pubKey) {
+	               		object = obj[pubKey] = obj[key];
+	              		delete obj[key];
+		       		} else {
+		       			object = obj[key];
+		       		}
+
+	       			if (_.isObject(object)) {
+		   				changePrivToPub(object);
+		       		}
+		        }
+		    }
+		}
+	}
+
+	var sendError = function (err) {
 		res.json(500, {
 			status: false,
 			error: err
@@ -35,6 +67,11 @@ exports.all = function(req, res) {
 				.onResolve(function(err, conditions) {
 					if (err) sendError(err);
 
+					for (var i = 0; i < platforms.length; i++) {
+						platforms[i] = platforms[i].toObject();
+					}
+					
+					changePrivToPub(platforms);
 					res.json({
 						status: true,
 						error: null,
