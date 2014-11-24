e88ff8e2e5d5167a4540040422fb51815db0aa45
Laurens Coderre <lcoderre@me.com>
2014-11-24 17:26:25 -0500

    fix stats return

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -6,23 +6,21 @@ var Stats = mongoose.model('Stats');
 exports.read = function(req, res) {
 	var columns = 'id data type timestamp';
 
-	if (!req.query.deviceId) {
-		Stats.find({
-			timestamp: {"$gte": req.query.from || 0, "$lt": req.query.to || Date.now()}
-		})
-		.select(columns)
-		.exec(function (err, stats) {
-			res.json(stats);
-		});
-	} else {
-		Stats.find({ 
+	Stats.find(
+		req.query.deviceId ? 
+		{ 
 			id: req.query.deviceId,
 			timestamp: {"$gte": req.query.from || 0, "$lt": req.query.to || Date.now()} 
+		} :
+		{
+			timestamp: {"$gte": req.query.from || 0, "$lt": req.query.to || Date.now()}
 		})
-		.select(columns)
-		.exec(function (err, stats) {
-			if (err) console.log('error1');
-			res.json(stats);
+	.select(columns)
+	.exec(function (err, stats) {
+		res.json({
+			status: !!err,
+			error: err,
+			statistics: stats
 		});
-	}
+	});
 };
