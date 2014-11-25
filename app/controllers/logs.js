98209e56a83aaa27a2fd26175a9ac427fb780fc2
Laurens Coderre <lcoderre@me.com>
2014-11-25 11:17:34 -0500

    Fix logs/history

--- app/controllers/logs.js
+++ app/controllers/logs.js
@@ -10,7 +10,19 @@ exports.read = function(req, res) {
 		timestamp: {"$gte": req.query.from || 0, "$lt": req.query.to || Date.now()}
 	})
 	.select(columns)
-	.exec(function (err, stats) {
-		res.json(stats);
+	.exec(function (err, logs) {
+		if (err) {
+			res.status(500).json({
+				status: !!err,
+				error: err,
+				history: logs
+			});
+		} else {
+			res.json({
+				status: !!err,
+				error: err,
+				history: logs
+			});
+		}
 	});
 };
