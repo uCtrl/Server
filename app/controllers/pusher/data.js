159563feecea34de3242bf47f85c95e0f5f808bc
Laurens Coderre <lcoderre@me.com>
2014-11-26 17:14:29 -0500

    Update device value

--- app/controllers/pusher/data.js
+++ app/controllers/pusher/data.js
@@ -16,15 +16,19 @@ exports.save = function(data) {
 	}
 
 	UDevice.findOne({tpId: deviceID})
-	.select('id name')
 	.exec(function (err, device) {
 		//console.log(device);
 		if (err || !device) { 
-			console.log ("data in from Pusher. Can't find the related device.", data);
+			console.log ("data in from Pusher. Can't find the related device.");//, data);
 			return;
 		}
 		
 		console.log("DATA from " + device.name + "  ("+deviceID+")"); 
+
+		device.value = data.DA;
+		device.lastUpdated = Date.now();
+		device.save();
+
 		var o = new Stats({
 			// Ninja equivalents
 			id: device.id,
