a33f0533a9e941c2ef666c1953099c323637d5a5
Laurens Coderre <lcoderre@me.com>
2014-12-02 10:17:39 -0500

    Fill leds

--- seed/stats.js
+++ seed/stats.js
@@ -89,8 +89,49 @@ module.exports = function() {
                 });
             },
 
-            function fillSomethingElse(devices, cb) {
-                console.log("fill something else");
+            function fillLEDS(devices, cb) {
+
+                var ids = [
+                '1014BBBK6089_3whiteH149ee01a96a_0_1012',
+                '1014BBBK6089_2whiteH149ee01a96a_0_1012',
+                '1014BBBK6089_1whiteH149ee01a96a_0_1012'];
+
+
+                async.each(ids, function(id, callback_id) {
+                    var led = _.find(devices, {tpId: id });
+
+
+                    var timestamps = _.map(days, function(d) {
+                        var start =    (d + hours[_.random(5,7)] + _.random(0, mins[20]));
+                        var fin =      (d + hours[_.random(9,11)] + _.random(0, mins[20]));
+                        var start2 =   (d + hours[_.random(16,18)] + _.random(0, mins[20]));
+                        var fin2 =     (d + hours[_.random(21,23)] + _.random(0, mins[20]));
+
+                        return [start, fin, start2, fin2];
+                    });
+                    timestamps = _.sortBy(_.map(_.flatten(timestamps), function(x) {
+                        return now - x;
+                    }));
+
+                    async.each(timestamps, function(t, callback_t) {
+                        var index = _.findIndex(timestamps, t) % 2;
+
+                        var o = new Stats({
+                            id: led.id,
+                            data: index? _.random(0,40) : _.random(200,250),
+                            type: 1012,
+                            timestamp: t
+                        });
+                        o.save(function(err, o){
+                            callback_t(err);
+                        });
+                    }, function (err) {
+                        if (err) callback_id(err);
+                        callback_id(null);
+                    });
+                }, function(err) {
+                    cb(err, devices);
+                });
 
             }],
 
@@ -98,10 +139,10 @@ module.exports = function() {
                 if (err) console.log ("Error seeding stats and logs", err);
                 console.log("Done seeding stats.");
             });
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+})
+.catch(function(err) {
+    console.log(err);
+});
 };
 
 
