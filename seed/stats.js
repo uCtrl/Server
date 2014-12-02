2f279051b020878eacef6daadb2add41c908b396
Laurens Coderre <lcoderre@me.com>
2014-12-02 00:01:37 -0500

    fill doors

--- seed/stats.js
+++ seed/stats.js
@@ -18,6 +18,8 @@ Logs = mongoose.model('Log');
 module.exports = function() {
 
 
+
+
     Q.ninvoke(mongoose,'connect', config.db)
     .then(function () {
         var now = Date.now();
@@ -28,8 +30,26 @@ module.exports = function() {
         }
         var mins = [];
         for (var i = 0; i < 60; i++) { 
-            hours.push(i * 1000);
+            mins.push(i * 1000);
         }
+
+        function makeTimestamps() {
+            var timestamps = _.map(days, function(d) {
+                return _.map(hours, function(h) {
+                    var rand = _.random(0,7);
+                    var wentBy = [];
+                    for (var i=0; i < rand; i++) {
+                        wentBy[i] = _.random(0, mins[20]) + h + d;
+                    }
+                    return wentBy;
+                });
+            });
+            timestamps = _.sortBy(_.map(_.flatten(timestamps), function(x) {
+                return now - x;
+            }));
+            return timestamps;
+        }
+
 
         async.waterfall([
             function getDevices(cb){
@@ -39,48 +59,45 @@ module.exports = function() {
             },
 
             function fillDoors(devices, cb) {
-                var door = _.find(devices, {tpId: '1014BBBK6089_0_0_11:111101010101011101010000'});
+                var ids = [
+                    '1014BBBK6089_0_0_11:111101010101011101010000',
+                    '1014BBBK6089_0_0_11:110100110101010100110000',
+                    '1014BBBK6089_0_0_11:010101011111000101010000',
+                    '1014BBBK6089_0_0_11:010111010100011100110000',
+                    '1014BBBK6089_0_0_11:010000010101110101010000',
+                    '1014BBBK6089_0_0_11:110111011101010101010000'];
 
-                var timestamps = _.map(days, function(d) {
-                    return _.map(hours, function(h) {
-                        var rand = _.random(0,7);
-                        var wentBy = [];
-                        for (var i=0; i < rand; i++) {
-                            wentBy[i] = _.random(0, hours[1]) + h + d;
-                        }
-                        return wentBy;
+                async.each(ids, function(id, callback_id) {
+                    var door = _.find(devices, {tpId: id });
+                    var timestamps = makeTimestamps();
+                    async.each(timestamps, function(t, callback_t) {
+                        var o = new Stats({
+                            id: door.id,
+                            data: door.value,
+                            type: 11,
+                            timestamp: t
+                        });
+                        o.save(function(err, o){
+                            callback_t(err);
+                        });
+                    }, function (err) {
+                        if (err) callback_id(err);
+                        callback_id(null);
                     });
+                }, function(err) {
+                    cb(err, devices);
                 });
-
-                timestamps = _.sortBy(_.map(_.flatten(timestamps), function(x) {
-                    return now - x;
-                }));
-
-                async.each(timestamps, function(t, callback) {
-                    var o = new Stats({
-                        id: door.id,
-                        data: door.value,
-                        type: 11,
-                        timestamp: t
-                    });
-                    o.save(function(err, o){
-                        callback(err);
-                    });
-                }, function (err) {
-                    if (err) cb(err);
-
-                    cb(null, devices);
-                });
-            }, 
+            },
 
             function fillSomethingElse(devices, cb) {
-                console.log("yeah");
+                console.log("fill something else");
+
             }],
 
             function(err, results) {
                 if (err) console.log ("Error seeding stats and logs", err);
-            }
-        );
+                console.log("Done seeding stats.");
+            });
     })
     .catch(function(err) {
         console.log(err);
